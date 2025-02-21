66193bbed132b9697ca2bc8b3af19f044e76be16
devops-contribution <38641966+devops-contribution@users.noreply.github.com>
2025-02-21 11:58:46 +0530

    Update writer.py

--- app/writer/writer.py
+++ app/writer/writer.py
@@ -1,12 +1,25 @@
 import time
 import mysql.connector
+import os
 from prometheus_client import start_http_server, Summary
 
 REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
 
+# MySQL credentials from environment variables
+MYSQL_USER = os.getenv('MYSQL_USER')
+MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+MYSQL_HOST = os.getenv('MYSQL_HOST')
+MYSQL_DATABASE = os.getenv('MYSQL_DATABASE')
+
 @REQUEST_TIME.time()
 def write_data():
-    conn = mysql.connector.connect(user='user', password='password', host='mysql-0.mysql.default.svc.cluster.local', database='test')
+    # Connect to MySQL using environment variables
+    conn = mysql.connector.connect(
+        user=MYSQL_USER, 
+        password=MYSQL_PASSWORD, 
+        host=MYSQL_HOST, 
+        database=MYSQL_DATABASE
+    )
     cursor = conn.cursor()
     cursor.execute("INSERT INTO data (value) VALUES (%s)", (time.time(),))
     conn.commit()
