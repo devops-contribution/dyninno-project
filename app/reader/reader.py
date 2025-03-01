ad10a8118605c8307c57140b21dfc07f4f557a10
Mukesh <mukesh@EMPID24089.local>
2025-03-01 21:21:16 +0530

    update

--- app/reader/reader.py
+++ app/reader/reader.py
@@ -1,3 +1,5 @@
+"""This module handles reading data from MySQL and exposing metrics."""
+
 import os
 import mysql.connector
 from prometheus_client import start_http_server, Summary
@@ -14,7 +16,7 @@ MYSQL_DATABASE = os.getenv('MYSQL_DATABASE')
 
 @REQUEST_TIME.time()
 def read_data():
-    # Connect to MySQL using environment variables
+    """ Connect to MySQL using environment variables. """
     conn = mysql.connector.connect(
         user=MYSQL_USER,
         password=MYSQL_PASSWORD,
@@ -31,6 +33,14 @@ def read_data():
 
 @app.route('/')
 def index():
+    """Returns the row count from the database and the current pod name.
+
+    Fetches the row count using `read_data()` and includes the pod's 
+    name for identification.
+
+    Returns:
+        str: A formatted string containing the row count and pod name.
+    """
     count = read_data()
     return f"Row count: {count}, Pod Name: {__name__}"
 
